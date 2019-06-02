e958b86a237c8a608e25a0fa5a0a0be7da82e478
jardenliu <15915839707@163.com>
2019-06-02 23:28:54 +0800

    🌈added app banner

--- src/components/macos/sidebar.tsx
+++ src/components/macos/sidebar.tsx
@@ -1,29 +1,49 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
-import { Platform, StyleSheet, Text, View, Dimensions } from "react-native";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Image
+} from 'react-native'
 
-type Props = {};
+type Props = {}
 
 export default class Sidebar extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.logo}>
-          <Text>Logo</Text>
+        <View style={styles.banner}>
+          <Image
+            source={require('@/assets/logo/banner@400.png')}
+            style={styles.bannerImage}
+          />
         </View>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     width: 250,
-    flexDirection: "row",
-    backgroundColor: "#f5f5f5",
-    alignSelf: "stretch"
+    flexDirection: 'row',
+    backgroundColor: '#f5f5f5',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    alignSelf: 'stretch'
   },
-  logo: {
-    marginTop: 25
+  banner: {
+    marginTop: 40,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  bannerImage: {
+    width: 1148 / 10,
+    height: 400 / 10,
+    resizeMode: 'stretch'
   }
-});
+})
