a807f5ee0c75d3092c2e36288804620f7f7fd833
jardenliu <15915839707@163.com>
2019-06-13 16:32:47 +0800

    remove console.log

--- src/shim/fetch.js
+++ src/shim/fetch.js
@@ -1,13 +1,11 @@
 module.exports = function(settings, cb) {
   fetch(settings.url, settings)
     .then(res => {
-      console.log(res)
       let response = {
         headers: res.headers.map,
         body: res._bodyInit,
         statusCode: res.status
       }
-      console.log('TCL: response', response)
 
       if (cb && typeof cb === 'function') {
         cb(null, response, response.body)
