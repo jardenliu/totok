bcc4ff92d2920473bf52e6a8974cf672b0125b71
jardenliu <15915839707@163.com>
2019-06-13 16:30:46 +0800

    added fetch shim for request

--- src/shim/fetch.js
+++ src/shim/fetch.js
@@ -1,1 +1,21 @@
-module.exports = fetch
+module.exports = function(settings, cb) {
+  fetch(settings.url, settings)
+    .then(res => {
+      console.log(res)
+      let response = {
+        headers: res.headers.map,
+        body: res._bodyInit,
+        statusCode: res.status
+      }
+      console.log('TCL: response', response)
+
+      if (cb && typeof cb === 'function') {
+        cb(null, response, response.body)
+      }
+    })
+    .catch(e => {
+      if (cb && typeof cb === 'function') {
+        cb(e)
+      }
+    })
+}
