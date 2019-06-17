39575d0cdad2e9323041cff15b8fa18bb6e58351
jardenliu <15915839707@163.com>
2019-06-17 23:58:37 +0800

    default theme config

---
+++ src/styles/themes/default.tsx
@@ -0,0 +1,16 @@
+// Color
+export const ColorPrimary = '#409EFF'
+export const ColorSuccess = '#67C23A'
+export const ColorWarning = '#E6A23C'
+export const ColorDanger = '#F56C6C'
+export const ColorInfo = '#909399'
+// Text Color
+export const ColorPrimaryText = '#303133'
+export const ColorRegularText = '#606266'
+export const ColorSecondaryText = '#909399'
+export const ColorPlaceholderText = '#C0C4CC'
+// Border Color
+export const ColorBorderBase = '#DCDFE6'
+export const ColorBorderLight = '#E4E7ED'
+export const ColorBorderLighter = '#EBEEF5'
+export const ColorBorderExtraLight = '#F2F6FC'
